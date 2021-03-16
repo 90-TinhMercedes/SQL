a422ba5bc4e745e04d408350d57d4fa930860ae6
Chí Tình <chitinhhaui@gmail.com>
2021-03-16 16:53:25 +0700

    SQL Update

---
+++ Practice/Revision 26 files/Revision_01.sql
@@ -0,0 +1,80 @@
+﻿create database QLSinhVien_De1
+
+use QLSinhVien_De1
+
+create table Khoa (
+	MaKhoa char(10) not null primary key,
+	TenKhoa nvarchar(50)
+)
+
+create table Lop (
+	MaLop char(10) primary key,
+	TenLop nvarchar(50),
+	SiSo int,
+	MaKhoa char(10),
+	constraint FK_Lop foreign key (MaKhoa) references Khoa(MaKhoa) on update cascade on delete cascade,
+	--alter table Lop add constraint df_Lop default (0) for SiSo
+)
+
+alter table Lop add constraint df_Lop default (0) for SiSo
+
+create table SinhVien (
+	MaSV char(10) primary key,
+	HoTen nvarchar(50),
+	NgaySinh datetime,
+	GioiTinh bit,
+	MaLop char(10),
+	constraint FK_SinhVien foreign key (MaLop) references Lop(MaLop) on update cascade on delete cascade
+)
+
+insert into Khoa values('K01', 'Khoa 01')
+insert into Khoa values('K02', 'Khoa 02')
+
+
+insert into Lop values('L01', 'Lop 01', null, 'K01')
+update Lop set SiSo = 15 where MaLop = 'L01'
+insert into Lop values('L02', 'Lop 02', 25, 'K02')
+insert into Lop values('L03', 'Lop 03', 20, 'K01')
+
+insert into SinhVien values('SV01', 'Tinh Handsome', '03/06/2001', 0, 'L01')
+insert into SinhVien values('SV02', 'Tinh Mercedes', '12/07/2000', 0, 'L02')
+insert into SinhVien values('SV03', 'Tinh Veigar', '08/06/2001', 1, 'L01')
+insert into SinhVien values('SV04', 'Tinh Vjp', '11/25/1999', 1, 'L01')
+insert into SinhVien values('SV05', 'Tinh Renekton', '07/17/2000', 0, 'L02')
+insert into SinhVien values('SV06', 'Tinh Ashe', '03/16/2001', 1, 'L02')
+insert into SinhVien values('SV07', 'Tinh Yasuo', '06/27/2000', 0, 'L01')
+
+select * from Khoa
+select * from Lop
+select * from SinhVien
+
+-- Câu 2 (3 đ): Hãy tạo View đưa ra thống kê số lớp của từng khoa gồm các thông tin: TenKhoa, Số lớp.
+create view cau2 
+as 
+	select TenKhoa, count(MaLop) as 'MaLop' from Khoa inner join Lop on Khoa.MaKhoa = Lop.MaKhoa
+	group by TenKhoa
+	
+select * from cau2 
+
+
+-- Câu 3 (2đ): Viết hàm với tham số truyền vào là MaKhoa, hàm trả về một bảng gồm các thông tin:
+--MaSV, HoTen, NgaySinh, GioiTinh (là “Nam“ hoặc “Nữ“), TenLop, TenKhoa. 
+alter function cau3 (@MaKhoa char(10))
+returns @ThongKe table (MaSV char(10), HoTen nvarchar(50), NgaySinh datetime, GioiTinh nvarchar(50), TenLop nvarchar(50), TenKhoa nvarchar(50))
+as 
+	begin
+		insert into @ThongKe
+		select MaSV, HoTen, NgaySinh, case when GioiTinh = 0 then 'Nu' when GioiTinh = 1 then 'Nam' end,
+		TenLop, TenKhoa from Khoa inner join Lop on Khoa.MaKhoa = Lop.MaKhoa
+		inner join SinhVien on Lop.MaLop = SinhVien.MaLop
+		where Lop.MaKhoa = @MaKhoa
+		return
+	end
+--Test
+
+select * from Khoa
+select * from Lop
+select * from SinhVien
+select * from cau3('K01')
+select * from cau3('K02')
+
---
+++ Practice/Revision 26 files/Revision_02_Slide04.sql
@@ -0,0 +1,71 @@
+﻿create database QLBenhVien_Slide_04
+
+use QLBenhVien_Slide_04
+
+create table BenhVien (
+	MaBV char(10) primary key,
+	TenBV nvarchar(50),
+	DiaChi nvarchar(50),
+	DienThoai nvarchar(50)
+)
+
+create table KhoaKham (
+	MaKhoa char(10) primary key,
+	TenKhoa nvarchar(50),
+	SoBenhNhan int, 
+	MaBV char(10),
+	constraint fk_KhoaKham foreign key (MaBV) references BenhVien(MaBV) on update cascade on delete cascade
+)
+
+create table BenhNhan (
+	MaBN char(10) primary key,
+	HoTen nvarchar(50),
+	NgaySinh datetime,
+	GioiTinh nvarchar(50),
+	SoNgayNV int,
+	MaKhoa char(10),
+	constraint fk_BenhNhan foreign key (MaKhoa) references KhoaKham(MaKhoa) on update cascade on delete cascade
+)
+
+insert into BenhVien values('BV01', 'Benh Vien 01', 'Dia Chi 01', '045454545')
+insert into BenhVien values('BV02', 'Benh Vien 02', 'Dia Chi 02', '025252525')
+
+insert into KhoaKham values('K01', 'khoa 01', 15, 'BV01')
+insert into KhoaKham values('K02', 'khoa 02', 15, 'BV02')
+update KhoaKham set TenKhoa = 'Khoa 01' where MaKhoa = 'K01'
+update KhoaKham set TenKhoa = 'Khoa 02' where MaKhoa = 'K02'
+
+
+insert into BenhNhan values('BN01', 'Yasuo', '12/07/2000', 'Nam', 7, 'K01')
+insert into BenhNhan values('BN02', 'Zed', '10/27/1999', 'Nam', 6, 'K01')
+insert into BenhNhan values('BN03', 'Yone', '02/17/2000', 'Nu', 10, 'K02')
+insert into BenhNhan values('BN04', 'Master Yi', '05/08/2002', 'Nam', 3, 'K02')
+insert into BenhNhan values('BN05', 'Renekton', '10/06/2003', 'Nu', 5, 'K02')
+insert into BenhNhan values('BN06', 'Leblance', '08/05/1998', 'Nam', 7, 'K01')
+insert into BenhNhan values('BN07', 'Rengar', '07/25/1998', 'Nu', 7, 'K01')
+
+-- Cau 02: Tạo view đưa ra thống kê số bệnh nhân Nữ của từng khoa khám gồm: MaKhoa, TenKhoa, Số người.
+create view cau02
+as
+	select KhoaKham.MaKhoa, TenKhoa, count(MaBN) as 'So Nguoi'
+	from KhoaKham inner join BenhNhan on KhoaKham.MaKhoa = BenhNhan.MaKhoa
+	where GioiTinh = 'Nu'
+	group by KhoaKham.MaKhoa, TenKhoa
+
+select * from cau02
+
+--cau 03: tạo hàm đưa ra tổng số tiền thu được của từng khoa khám bệnh là bao nhiêu?
+--Tham số truyền vào là: TenKhoa, Tien = SoNgayNV * 60000
+
+create function cau03(@TenKhoa nvarchar(50))
+returns money
+as
+	begin
+		declare @SumPrice money
+		select @SumPrice = sum(SoNgayNV * 60000)
+		from KhoaKham inner join BenhNhan on KhoaKham.MaKhoa = BenhNhan.MaKhoa
+		where TenKhoa = @TenKhoa 
+		return @Sumprice
+	end
+
+select dbo.cau03('Khoa 01')
