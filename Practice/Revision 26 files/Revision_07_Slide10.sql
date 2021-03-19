ffded424bad976095b290fe39d780e9cca325790
Chí Tình <chitinhhaui@gmail.com>
2021-03-19 21:36:28 +0700

    SQL Update

---
+++ Practice/Revision 26 files/Revision_07_Slide10.sql
@@ -0,0 +1,85 @@
+﻿--Câu 1 (3đ): Tạo csdl QLBenhVien gồm 3 bảng: 
+--+ BenhVien(MaBV,TenBV)
+--+ KhoaKham(MaKhoa, TenKhoa, SoBenhNhan, MaBV)
+--+ BenhNhan(MaBN,HoTen,NgaySinh,GioiTinh(bit),SoNgayNV, MaKhoa)
+
+
+create database QLBenhVien_Slide10
+
+use QLBenhVien_Slide10
+
+create table BENHVIEN (
+	MaBV char(10) primary key,
+	TenBV nvarchar(50)
+)
+
+create table KHOAKHAM (
+	MaKhoa char(10) primary key,
+	TenKhoa nvarchar(50),
+	SoBenhNhan int,
+	MaBV char(10),
+	constraint fk_KHOAKHAM foreign key (MaBV) references BENHVIEN(MaBV) on update cascade on delete cascade
+)
+
+create table BENHNHAN (
+	MaBN char(10) primary key,
+	HoTen nvarchar(50),
+	NgaySinh datetime,
+	GioiTinh bit,
+	SoNgayNV int,
+	MaKhoa char(10),
+	constraint fk_BENHNHAN foreign key (MaKhoa) references KHOAKHAM(MaKhoa) on update cascade on delete cascade
+)
+
+insert into BENHVIEN values ('BV01', 'Benh vien 01')
+insert into BENHVIEN values ('BV02', 'Benh vien 02')
+
+insert into KHOAKHAM values ('K01', 'Khoa 01', 5, 'BV01')
+insert into KHOAKHAM values ('K02', 'Khoa 02', 8, 'BV01')
+insert into KHOAKHAM values ('K03', 'Khoa 03', 6, 'BV02')
+
+insert into BENHNHAN values ('BN01', 'Yasuo', '04/25/1994', 0, 7, 'K01')
+insert into BENHNHAN values ('BN02', 'Malphite', '06/05/1995', 1, 5, 'K02')
+insert into BENHNHAN values ('BN03', 'Yone', '10/21/1994', 0, 10, 'K03')
+insert into BENHNHAN values ('BN04', 'Ashe', '12/17/1993', 0, 15, 'K01')
+insert into BENHNHAN values ('BN05', 'Caitlyn', '06/25/1995', 1, 8, 'K01')
+insert into BENHNHAN values ('BN06', 'Draven', '04/12/1992', 1, 6, 'K02')
+insert into BENHNHAN values ('BN07', 'Aatrox', '06/25/1992', 1, 3, 'K03')
+insert into BENHNHAN values ('BN08', 'Renekton', '08/17/1991', 0, 5, 'K01')
+
+select * from BENHVIEN
+select * from KHOAKHAM
+select * from BENHNHAN
+
+--Câu 2 (2đ): Hãy tạo View đưa ra thống kê số bệnh nhân Nữ của từng khoa khám gồm các thông tin: MaKhoa, TenKhoa, Số_người.
+create view cau02
+as
+	select KHOAKHAM.MaKhoa, TenKhoa, count(MaBN) as SoNguoi
+	from KHOAKHAM inner join BENHNHAN on KHOAKHAM.MaKhoa = BENHNHAN.MaKhoa
+	group by KHOAKHAM.MaKhoa, TenKhoa
+
+select * from cau02
+
+--Câu 3 (2đ): Hãy tạo thủ tục lưu trữ in ra tổng số tiền thu được của từng khoa khám bệnh là bao nhiêu?
+--(Với tham số vào là: MaKhoa, Tien=SoNgayNV*80000).
+
+--Làm trước với phần tạo hàm.
+alter function cau03_function(@MaKhoa char(10))
+returns table
+as
+	return
+		select KHOAKHAM.MaKhoa, sum(SoNgayNV * 80000) as Tien
+		from KHOAKHAM inner join BENHNHAN on KHOAKHAM.MaKhoa = BENHNHAN.MaKhoa
+		where KHOAKHAM.MaKhoa = @MaKhoa
+		group by KHOAKHAM.MaKhoa 
+
+select * from BENHVIEN
+select * from KHOAKHAM
+select * from BENHNHAN
+select * from cau03_function('K01')
+select * from cau03_function('K02')
+
+
+
+
+
---
+++ Practice/Revision 26 files/Revision_08_Slide11.sql
@@ -0,0 +1,80 @@
+﻿--Câu 1 (3đ): Tạo csdl QLHANG gåm 3 bảng sau: 
+--+ Hang(MaHang,TenHang,DVTinh, SLTon)
+--+ HDBan(MaHD,NgayBan,HoTenKhach)
+--+ HangBan(MaHD,MaHang,DonGia,SoLuong)
+
+create database QLHang_Slide11
+
+use QLHang_Slide11
+
+create table HANG (
+	MaHang char(10) primary key,
+	TenHang nvarchar(50),
+	DVTinh nvarchar(50),
+	SLTon int
+)
+
+create table HDBAN (
+	MaHD char(10) primary key,
+	NgayBan datetime,
+	HoTenKhach nvarchar(50)
+)
+
+create table HANGBAN (
+	MaHD char(10),
+	MaHang char(10),
+	DonGia money,
+	SoLuong int,
+	constraint pk_HANGBAN primary key (MaHD, MaHang),
+	constraint first_fk_HangBan foreign key (MaHD) references HDBAN(MaHD) on update cascade on delete cascade,
+	constraint second_fk_HangBan foreign key (MaHang) references HANG(MaHang) on update cascade on delete cascade 
+)
+
+insert into HANG values ('H01', 'Hang 01', 'cai', 10)
+insert into HANG values ('H02', 'Hang 02', 'chiec', 15)
+
+insert into HDBAN values ('HD01', '12/25/2020', 'Tinh Mercedes')
+insert into HDBAN values ('HD02', '08/20/2019', 'Tinh Main Veigar')
+
+insert into HANGBAN values ('HD01', 'H01', 15000, 5)
+insert into HANGBAN values ('HD02', 'H02', 20000, 4)
+insert into HANGBAN values ('HD02', 'H01', 17000, 3)
+insert into HANGBAN values ('HD01', 'H02', 9000, 7)
+
+select * from HANG
+select * from HDBAN
+select * from HANGBAN
+
+--Câu 2 (2đ): Hãy tạo View đưa ra thống kê tiền hàng bán theo từng hóa đơn 
+--gồm: MaHD,NgayBan,Tổng tiền (tiền=SoLuong*DonGia)
+
+create view cau02
+as
+	select HANGBAN.MaHD, NgayBan, sum(SoLuong * DonGia) as TongTien
+	from HANGBAN inner join HDBAN on HANGBAN.MaHD = HDBAN.MaHD
+	group by HANGBAN.MaHD, NgayBan
+
+select * from cau02
+
+--Câu 3 (2đ): Hãy tạo hàm in ra tổng tiền hàng bán theo năm là bao nhiêu? (Với tham số vào là: Năm).
+create function cau03(@Nam int)
+returns int
+as
+	begin
+		declare @Result int
+		select @Result = sum(SoLuong * DonGia)
+		from HDBAN inner join HANGBAN on HDBAN.MaHD = HANGBAN.MaHD
+		where YEAR(NgayBan) = @Nam
+		return @Result
+	end
+
+select  dbo.cau03('2019') as TongTIen
+select  dbo.cau03('2020') as TongTIen
+select  dbo.cau03('2021') as TongTIen
+
+
+
+
+
+
+
