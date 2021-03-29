74fce2fceb61f81b4eb9e26c952ec7f57af6789c
Chí Tình <chitinhhaui@gmail.com>
2021-03-29 15:37:39 +0700

    SQL Update

---
+++ Learning/Trigger/TriggerBasic.sql
@@ -0,0 +1,63 @@
+﻿create database TriggerBasic
+
+use TriggerBasic
+
+create table TACGIA (
+	MaTG char(10) primary key,
+	TenTG nvarchar(50),
+	SoLuongCo int
+)
+
+create table NHAXB (
+	MaNXB char(10) primary key,
+	TenNXB nvarchar(50),
+	SoLuongCo int
+)
+
+create table SACH (
+	MaSach char(10) primary key,
+	TenSach nvarchar(50),
+	MaNXB char(10),
+	MaTG char(10),
+	NamXB int,
+	SoLuong int,
+	DonGia money
+	constraint fk_SACH_01 foreign key (MaNXB) references NHAXB(MaNXB) on update cascade on delete cascade,
+	constraint fk_SACH_02 foreign key (MaTG) references TACGIA(MaTG) on update cascade on delete cascade
+)
+
+insert into TACGIA values('TG01', 'Tac gia 01', 3)
+insert into TACGIA values('TG02', 'Tac gia 02', 5)
+insert into TACGIA values('TG03', 'Tac gia 03', 2)
+
+insert into NHAXB values('NXB01', 'Nha xuat ban 01', 4)
+insert into NHAXB values('NXB02', 'Nha xuat ban 02', 2)
+insert into NHAXB values('NXB03', 'Nha xuat ban 03', 5)
+
+insert into SACH values('SA01', 'Sach 01', 'NXB02', 'TG01', 2019, 20, 15000)
+insert into SACH values('SA02', 'Sach 02', 'NXB03', 'TG02', 2015, 15, 21000)
+insert into SACH values('SA03', 'Sach 03', 'NXB01', 'TG02', 2017, 17, 17000)
+insert into SACH values('SA04', 'Sach 04', 'NXB01', 'TG03', 2020, 9, 9000)
+insert into SACH values('SA05', 'Sach 05', 'NXB01', 'TG01', 2019, 14, 16000)
+insert into SACH values('SA06', 'Sach 06', 'NXB02', 'TG02', 2020, 22, 15500)
+
+select * from TACGIA
+select * from NHAXB
+select * from SACH
+
+-- tạo trigger insert
+create trigger userInsertTACGIA on TACGIA
+for insert, update
+as
+	begin
+		print 'Insert successful!'
+	end
+
+
+
+
+
+
+
+
+
---
+++ Practice/Revision 26 files/Revision_10_Slide13.sql
@@ -0,0 +1,46 @@
+﻿--Câu 1 (3đ): Tạo csdl QLHANG gåm 3 bảng sau: 
+--+ Hang(MaHang,TenHang,DVTinh, SLTon)
+--+ HDBan(MaHD,NgayBan,HoTenKhach)
+--+ HangBan(MaHD,MaHang,DonGia,SoLuong)
+
+create database QLHANG_Slide13
+
+use QLHANG_Slide13
+
+create table HANG (
+	MaHang char(10) primary key,
+	TenHang nvarchar(50),
+	DVTinh nvarchar(50),
+	SLTon int
+)
+
+create table HDBAN (
+	MaHD char(10) primary key,
+	NgayBan datetime,
+	HoTenKhach nvarchar(50)
+)
+
+create table HANGBAN (
+	MaHD char(10),
+	MaHang char(10),
+	DonGia money,
+	SoLuong int,
+	constraint pk_HANGBAN primary key (MaHD, MaHang),
+	constraint first_fk_HangBan foreign key (MaHD) references HDBAN(MaHD) on update cascade on delete cascade,
+	constraint second_fk_HangBan foreign key (MaHang) references HANG(MaHang) on update cascade on delete cascade 
+)
+
+insert into HANG values ('H01', 'Hang 01', 'cai', 10)
+insert into HANG values ('H02', 'Hang 02', 'chiec', 15)
+
+insert into HDBAN values ('HD01', '12/25/2020', 'Tinh Mercedes')
+insert into HDBAN values ('HD02', '08/20/2019', 'Tinh Main Veigar')
+
+insert into HANGBAN values ('HD01', 'H01', 15000, 5)
+insert into HANGBAN values ('HD02', 'H02', 20000, 4)
+insert into HANGBAN values ('HD02', 'H01', 17000, 3)
+insert into HANGBAN values ('HD01', 'H02', 9000, 7)
+
+select * from HANG
+select * from HDBAN
+select * from HANGBAN
