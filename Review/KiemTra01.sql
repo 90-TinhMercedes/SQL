52b746ce45758983bf7c26d03c60a9b51d573103
Chí Tình <chitinhhaui@gmail.com>
2021-04-17 13:19:23 +0700

    SQL Update

---
+++ Review/KiemTra01.sql
@@ -0,0 +1,90 @@
+﻿create database de_kiem_tra_01
+
+use de_kiem_tra_01
+
+create table BENHVIEN (
+	MaBV char(10) primary key,
+	TenBV nvarchar(50),
+	DiaChi nvarchar(50),
+	DienThoai int
+)
+
+create table KHOAKHAM (
+	MaKhoa char(10) primary key,
+	TenKhoa nvarchar(50),
+	SoBenhNhan int,
+	MaBV char(10),
+	constraint FK_KHOAKHAM foreign key (MaBV) references BENHVIEN(MaBV) on update cascade on delete cascade
+)
+
+create table BENHNHAN (
+	MaBN char(10) primary key,
+	HoTen nvarchar(50),
+	NgaySinh date,
+	GioiTinh bit,
+	SoNgayNV int,
+	MaKhoa char(10),
+	DienThoai varchar(15),
+	constraint FK_BENHNHAN foreign key (MaKhoa) references KHOAKHAM(MaKhoa) on update cascade on delete cascade
+)
+
+insert into BENHVIEN values ('BV01', 'Benh vien 01', 'Dia chi 01', 457878788)
+insert into BENHVIEN values ('BV02', 'Benh vien 02', 'Dia chi 02', 121212121)
+
+insert into KHOAKHAM values ('K01', 'Khoa 01', 20, 'BV01')
+insert into KHOAKHAM values ('K02', 'Khoa 02', 30, 'BV02')
+
+insert into BENHNHAN values ('BN01', 'Benh nhan 01', '04/17/2000', 0, 10, 'K01', '0454545454')
+insert into BENHNHAN values ('BN02', 'Benh nhan 02', '04/17/2000', 0, 5, 'K02', '1212121212')
+insert into BENHNHAN values ('BN03', 'Benh nhan 03', '04/17/2000', 1, 15, 'K01', '36363636')
+insert into BENHNHAN values ('BN04', 'Benh nhan 04', '04/17/2000', 1, 5, 'K02', '0454545454')
+insert into BENHNHAN values ('BN05', 'Benh nhan 05', '04/17/2000', 0, 20, 'K01', '0454545454')
+
+select * from BENHVIEN
+select * from KHOAKHAM
+select * from BENHNHAN
+
+-- cau 02:
+create procedure cau02 (@tenKhoa nvarchar(50))
+as
+	begin
+		select SUM(SoNgayNV * 120000) as TongTien 
+		from BENHNHAN inner join KHOAKHAM on BENHNHAN.MaKhoa = KHOAKHAM.MaKhoa
+		where TenKhoa = @tenKhoa
+	end
+
+
+select * from KHOAKHAM
+select * from BENHNHAN
+execute cau02 'Khoa 01'
+execute cau02 'Khoa 02'
+select * from KHOAKHAM
+select * from BENHNHAN
+
+--cau 03:
+alter trigger cau03
+on BENHNHAN
+for insert
+as
+	begin
+		declare @soLuongKyTuSDT int
+		select @soLuongKyTuSDT = LEN(DienThoai) from inserted
+		if (@soLuongKyTuSDT != 10)
+			begin
+				raiserror (N'Số điện thoại bênh nhân không hợp lệ.', 16, 1)
+				rollback transaction
+			end
+	end
+
+select * from BENHNHAN
+--insert into BENHNHAN values ('BN06', 'Benh nhan 06', '04/17/2000', 0, 10, 'K01', 0454545) -- số điện thoại không hợp lệ
+insert into BENHNHAN values ('BN06', 'Benh nhan 06', '04/17/2000', 0, 10, 'K01', '4121223235') -- số điện thoại hợp lệ
+select * from BENHNHAN
+
+
+
+
+
+
+
+
---
+++ TestConditions_02/Code_24.sql
@@ -0,0 +1,106 @@
+﻿create database QLDA_Code24
+
+use QLDA_Code24
+
+create table NHANVIEN (
+	MaNV char(10) primary key,
+	TenNV nvarchar(50),
+	GioiTinh nvarchar(50),
+	HeSoLuong float
+)
+
+create table DUAN (
+	MaDA char(10) primary key,
+	TenDA nvarchar(50),
+	NgayBatDau date,
+	SoLuongNV int
+)
+
+create table THAMGIA (
+	MaNV char(10),
+	MaDA char(10),
+	NhiemVu nvarchar(50),
+	constraint PK_THAMGIA primary key (MaNV, MaDA),
+	constraint FK_THAMGIA_01 foreign key (MaNV) references NHANVIEN(MaNV) on update cascade on delete cascade,
+	constraint FK_THAMGIA_02 foreign key (MaDA) references DUAN(MaDA) on update cascade on delete cascade
+)
+
+insert into NHANVIEN values ('NV01', 'Nhan vien 01', 'Nam', 2.5)
+insert into NHANVIEN values ('NV02', 'Nhan vien 02', 'Nam', 1.7)
+insert into NHANVIEN values ('NV03', 'Nhan vien 03', 'Nu', 2.2)
+
+insert into DUAN values ('DA01', 'Du an 01', '04/17/2021', 2)
+insert into DUAN values ('DA02', 'Du an 02', '04/17/2021', 2)
+insert into DUAN values ('DA03', 'Du an 03', '04/17/2021', 3)
+
+insert into THAMGIA values ('NV01', 'DA01', 'Nhiem vu 01')
+insert into THAMGIA values ('NV01', 'DA02', 'Nhiem vu 02')
+insert into THAMGIA values ('NV02', 'DA01', 'Nhiem vu 03')
+insert into THAMGIA values ('NV03', 'DA02', 'Nhiem vu 04')
+insert into THAMGIA values ('NV01', 'DA03', 'Nhiem vu 05')
+insert into THAMGIA values ('NV02', 'DA03', 'Nhiem vu 06')
+insert into THAMGIA values ('NV03', 'DA03', 'Nhiem vu 07')
+
+select * from NHANVIEN
+select * from DUAN
+select * from THAMGIA
+
+-- Cau 02:
+alter procedure cau02 (@maNV char(10), @heSoLuong float)
+as
+	begin
+		declare @heSoLuongHienTai float
+		select @heSoLuongHienTai = (select HeSoLuong from NHANVIEN where MaNV = @maNV)
+		if (not exists (select * from NHANVIEN where @maNV = MaNV))
+			begin
+				print N'Mã nhân viên ' + @maNV + N' không tồn tại.'
+			end
+		else if (@heSoLuongHienTai > @heSoLuong)
+			begin
+				print N'Hệ số lương vừa nhập nhỏ hơn Hệ số lương hiện tại. Không cho phép update.'
+			end
+		else
+			update NHANVIEN set HeSoLuong = @heSoLuong where MaNV = @maNV
+	end
+
+select * from NHANVIEN
+execute cau02 'NV05', '3.0' -- Mã nhân viên không tồn tại. Loại
+select * from NHANVIEN
+
+-- Cau 03:
+
+create trigger cau03
+on THAMGIA
+for insert
+as
+	begin
+		declare @heSoLuongNhanVien float
+		declare @maDuAn char(10)
+		declare @soLuongNhanVienTrongDuAn int
+		select @heSoLuongNhanVien = HeSoLuong, @maDuAn = inserted.MaDA 
+		from inserted inner join DUAN on DUAN.MaDA = inserted.MaDA
+		inner join NHANVIEN on NHANVIEN.MaNV = inserted.MaNV
+		select @soLuongNhanVienTrongDuAn = COUNT(MaDA) from THAMGIA where MaDA = @maDuAn 
+
+
+
+	end
+
+select * from NHANVIEN
+select * from DUAN
+select * from THAMGIA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
