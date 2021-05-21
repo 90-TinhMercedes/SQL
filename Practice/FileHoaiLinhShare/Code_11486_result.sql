fffe41ec2568a4cdac2da7cf779c5763aa761d8d
Chí Tình <65107706+90-TinhMercedes@users.noreply.github.com>
2021-05-21 15:58:34 +0700

    SQL Update

--- Practice/FileHoaiLinhShare/Code_11486_result.sql
+++ Practice/FileHoaiLinhShare/Code_11486_result.sql
@@ -1,4 +1,4 @@
-create database FileHoaiLinhShare_Code11486
+﻿create database FileHoaiLinhShare_Code11486
 
 use FileHoaiLinhShare_Code11486
 
@@ -44,7 +44,70 @@ select * from CONGTY
 select * from SANPHAM
 select * from CUNGUNG
 
+-- cau 02:
+create function cau02(@tenCongTy nvarchar(50), @ngayCungUng date)
+returns table
+as
+	return	
+		select TenSanPham, SoLuongCo, GiaBan
+		from CUNGUNG inner join SANPHAM on CUNGUNG.MaSanPham = SANPHAM.MaSanPham
+		inner join CONGTY on CUNGUNG.MaCongTy = CONGTY.MaCongTy
+		where TenCongTy = @tenCongTy and NgayCungUng = @ngayCungUng
+
+select * from CONGTY
+select * from SANPHAM
+select * from CUNGUNG
+select * from cau02('Cong Ty 01', '10/25/2020') 
+select * from cau02('Cong Ty 02', '03/17/2021') 
+
+-- cau 03:
+create procedure cau03(@maCongTy char(10), @tenCongTy nvarchar(50), @diaChi nvarchar(50), @result int output)
+as
+	begin
+		if(exists (select * from CONGTY where TenCongTy = @tenCongTy))
+			begin
+				print N'Tên công ty đã tồn tại.'
+				set @result = 1
+			end
+		else
+			begin
+				insert into CONGTY values (@maCongTy, @tenCongTy, @diaChi)
+				set @result = 0
+			end
+	end
+
+select * from CONGTY
+declare @ketQua int
+--execute cau03 'CTY05', 'Cong Ty 01', 'Dia chi 05', @ketQua output -- Tên công ty đã tồn tại. Không hợp lệ.
+execute cau03 'CTY05', 'Cong Ty 05', 'Dia chi 05', @ketQua output -- Tên công ty hợp lệ.
+select @ketQua as KetQua
+select * from CONGTY
 
 
+-- cau 04:
+create trigger cau04 on CUNGUNG
+for update
+as
+	begin
+		declare @soLuongCungUngNew int, @soLuongCungUngOld int, @maSanPhamCungUngUpdate char(10)
+		declare @soLuongCo int
+		select @soLuongCungUngNew = SoLuongCungUng, @maSanPhamCungUngUpdate = MaSanPham from inserted
+		select @soLuongCungUngOld = SoLuongCungUng from deleted
+		select @soLuongCo = SoLuongCo from SANPHAM where MaSanPham = @maSanPhamCungUngUpdate
+		if ((@soLuongCungUngNew - @soLuongCungUngOld) > @soLuongCo)
+			begin
+				raiserror(N'Số lượng trong kho không đủ để cung ứng.', 16, 1)
+				rollback transaction
+			end
+		else
+			update SANPHAM set SoLuongCo = SoLuongCo - (@soLuongCungUngNew - @soLuongCungUngOld)
+			where MaSanPham = @maSanPhamCungUngUpdate
 
+	end
 
+select * from SANPHAM
+select * from CUNGUNG
+--update CUNGUNG set SoLuongCungUng = 500 where MaCongTy = 'CTY01' and MaSanPham = 'SP01' --Số lượng cung ứng quá lớn, trong kho không đủ.
+update CUNGUNG set SoLuongCungUng = 30 where MaCongTy = 'CTY01' and MaSanPham = 'SP01' --Số lượng cung ứng hợp lệ
+select * from SANPHAM
+select * from CUNGUNG
