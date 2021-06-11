2b9bd75b711ee14d71f2f9e8caba10c6377cf2a0
Chí Tình <65107706+90-TinhMercedes@users.noreply.github.com>
2021-06-11 11:29:50 +0700

    Final Test Support

---
+++ Final Test/Support/CaoNam_SQL.sql
@@ -0,0 +1,109 @@
+﻿-- BÀI LÀM
+-- câu 01:
+-- a,
+create database QLThuVien
+
+use QLThuVien 
+
+create table DOCGIA (
+	MaDG char(10) primary key,
+	TenDG nvarchar(50),
+	DiaChi nvarchar(50),
+	SoDT nvarchar(50)
+)
+
+
+create table SACH (
+	MaS char(10) primary key,
+	TenS nvarchar(50),
+	SoLuong int,
+	NhaXB nvarchar(50),
+	NamXB int
+)
+
+create table PHIEUMUON (
+	MaPM char(10) primary key,
+	MaS char(10),
+	MaDG char(10),
+	SoLuongMuon int,
+	NgayM date,
+	constraint fk_PHIEUMUON_01 foreign key (MaS) references SACH(MaS) on update cascade on delete cascade,
+	constraint fk_PHIEUMUON_02 foreign key (MaDG) references DOCGIA(MaDG) on update cascade on delete cascade
+)
+
+--b,
+
+insert into DOCGIA values ('DG01', 'Doc gia 01', 'Dia chi 01', '089898989')
+insert into DOCGIA values ('DG02', 'Doc gia 02', 'Dia chi 02', '045454545')
+insert into DOCGIA values ('DG03', 'Doc gia 03', 'Dia chi 03', '056565656')
+
+insert into SACH values ('SA01', 'Sach 01', 200, 'Nha xuat ban 01', 2020)
+insert into SACH values ('SA02', 'Sach 02', 150, 'Nha xuat ban 02', 2021)
+insert into SACH values ('SA03', 'Sach 03', 100, 'Nha xuat ban 03', 2019)
+
+insert into PHIEUMUON values ('PM01', 'SA01', 'DG01', 5, '08/17/2020')
+insert into PHIEUMUON values ('PM02', 'SA02', 'DG02', 10, '05/28/2020')
+insert into PHIEUMUON values ('PM03', 'SA02', 'DG03', 5, '06/27/2020')
+insert into PHIEUMUON values ('PM04', 'SA01', 'DG01', 15, '11/16/2019')
+insert into PHIEUMUON values ('PM05', 'SA03', 'DG02', 20, '08/17/2020')
+
+select * from DOCGIA
+select * from SACH
+select * from PHIEUMUON
+
+-- câu 02:
+create function cau02(@maDocGia char(10))
+returns int
+as
+	begin
+		declare @tongSoLuongMuon int
+		select @tongSoLuongMuon = SUM(SoLuongMuon) from PHIEUMUON where MaDG = @maDocGia
+		return @tongSoLuongMuon
+	end
+
+select * from PHIEUMUON
+select dbo.cau02('DG01') as TongSoLuong -- Độc giả có mã DG01 mượn 20 quyển
+select dbo.cau02('DG02') as TongSoLuong -- Độc giả có mã DG02 mượn 30 quyển
+
+-- câu 03:
+create procedure cau03(@maPM char(10), @tenSach nvarchar(50), @tenDocGia nvarchar(50), @soLuongM int, @ngayM date)
+as
+	begin
+		declare @maSachMuon char(10), @maDocGiaMuon char(10)
+		if (not exists (select * from SACH where TenS = @tenSach) or not exists (select * from DOCGIA where TenDG = @tenDocGia))
+			begin
+				print N'Không tồn tại tên sách hoặc không tồn tại tên độc giả, không thể thêm PHIEUMUON mới.'
+			end
+		else
+			begin
+				select @maSachMuon = MaS from SACH where TenS = @tenSach
+				select @maDocGiaMuon = MaDG from DOCGIA where TenDG = @tenDocGia
+				insert into PHIEUMUON values (@maPM, @maSachMuon, @maDocGiaMuon, @soLuongM, @ngayM)
+			end
+	end
+
+select * from DOCGIA
+select * from SACH
+select * from PHIEUMUON
+--execute cau03 'PM06', 'Sach 100', 'Doc gia 100', 25, '04/25/2020' -- Tên sách và tên độc giả không tồn tại.
+--execute cau03 'PM06', 'Sach 100', 'Doc gia 01', 25, '04/25/2020' -- Tên sách hoặc tên độc giả không tồn tại.
+execute cau03 'PM06', 'Sach 03', 'Doc gia 03', 25, '04/25/2020' -- Hợp lệ
+select * from PHIEUMUON
+
+-- câu 04:
+create trigger cau01 on PHIEUMUON for delete
+as
+	begin
+		declare @maSachDelete char(10), @soLuongDelete int
+		select @maSachDelete = MaS, @soLuongDelete = SoLuongMuon from deleted
+		update SACH set SoLuong = SoLuong + @soLuongDelete where MaS = @maSachDelete
+	end
+
+select * from SACH
+select * from PHIEUMUON
+--delete PHIEUMUON where MaPM = 'PM01' -- Xoá PM01, cập nhật lại số lượng sách có mã SA01: 200 + 5 = 205
+delete PHIEUMUON where MaPM = 'PM02' -- Xoá PM02, cập nhật lại số lượng sách có mã SA02: 150 + 10 = 160
+select * from SACH
+select * from PHIEUMUON
+
+
---
+++ Training/CheckForLinh_Nguyễn-Hoài-Linh_2019605257_005.sql
@@ -0,0 +1,112 @@
+﻿create database QLTruongHoc
+go
+use QLTruongHoc
+go
+
+create table GiaoVien(
+	MaGV char(10) primary key,
+	TenGV nvarchar(50)
+)
+go
+create table Lop(
+	MaLop varchar(10) primary key,
+	TenLop nvarchar(50),
+	Phong char(10),
+	SiSo int,
+	MaGV char(10),
+	constraint FK_Lop foreign key (MaGV) references GiaoVien(MaGV) on update cascade on delete cascade
+)
+go
+create table SinhVien(
+	MaSV varchar(10) primary key,
+	TenSV nvarchar(50),
+	GioiTinh nvarchar(50),
+	QueQuan nvarchar(50),
+	MaLop varchar(10),
+	constraint FK_SinhVien foreign key (MaLop) references Lop(MaLop) on update cascade on delete cascade
+)
+go
+
+insert into GiaoVien values ('GV01',N'Nguyễn Thị A')
+insert into GiaoVien values ('GV02',N'Nguyễn Thị B')
+insert into GiaoVien values ('GV03',N'Nguyễn Văn C')
+
+insert into Lop values ('L01',N'Lớp 1','P01',30,'GV01')
+insert into Lop values ('L02',N'Lớp 2','P02',40,'GV02')
+insert into Lop values ('L03',N'Lớp 3','P03',50,'GV03')
+
+insert into SinhVien values ('SV01', N'Nguyễn Ngọc A',N'nữ', N'Hà Tĩnh','L01')
+insert into SinhVien values ('SV02', N'Nguyễn Ngọc B',N'nữ', N'Hà Nội','L02')
+insert into SinhVien values ('SV03', N'Nguyễn Ngọc C',N'nam', N'Hà Nam','L03')
+insert into SinhVien values ('SV04', N'Nguyễn Ngọc D',N'nữ', N'Hà Giang','L01')
+insert into SinhVien values ('SV05', N'Nguyễn Ngọc E',N'nam', N'Hà Tĩnh','L02')
+
+select *from GiaoVien
+select *from Lop
+select *from SinhVien
+--câu 2
+create function cau2 (@tenlop nvarchar(50), @tengv nvarchar(50))
+returns @banga table (MaSV char(10),
+						TenSV nvarchar(50),
+						GioiTinh nvarchar(50),
+						QueQuan nvarchar(50))
+as
+begin
+	insert into @banga
+	select MaSV, TenSV, GioiTinh, QueQuan
+	from SinhVien inner join Lop on SinhVien.MaLop=Lop.MaLop
+					inner join GiaoVien on GiaoVien.MaGV=Lop.MaGV
+	where TenLop=@tenlop and TenGV=@tengv
+	return
+end
+
+select *from GiaoVien
+select *from Lop
+select *from SinhVien
+select *from cau2 (N'Lớp 1', N'Nguyễn Thị A')
+select *from cau2 (N'Lớp 2', N'Nguyễn Thị B')
+
+--câu 3
+create procedure cau03(@maSV char(10), @tenSV nvarchar(50), @gioiTinh nvarchar(50), @queQuan nvarchar(50), @tenLop nvarchar(50))
+as
+	begin
+		declare @maLopThemSinhVien char(10)
+		if (not exists (select * from LOP where TenLop = @tenLop))
+			begin
+				print N'Tên lớp: ' + @tenLop + N' không tồn tại.'
+			end
+		else
+			begin
+				select @maLopThemSinhVien = MaLop from LOP where TenLop = @tenLop
+				insert into SINHVIEN values (@maSV, @tenSV, @gioiTinh, @queQuan, @maLopThemSinhVien)
+			end
+	end
+
+
+select * from Lop
+select * from SinhVien
+execute cau03 'SV06', N'Nguyễn Ngọc F', N'nữ', N'Hà Nội', N'Lớp 4' -- Tên lớp: Lớp 4 không tồn tại
+execute cau03 'SV06', N'Nguyễn Ngọc F', N'nữ', N'Hà Nội', N'Lớp 3' -- Tên lớp: Lớp 3 tồn tại. Hợp lệ
+select * from Lop
+select * from SinhVien
+
+
+--câu 4
+create trigger cau04 on SinhVien
+for update
+as
+	begin
+		declare @maLopCu char(10), @maLopMoi char(10), @siSoLopCu int, @siSoLopMoi int
+		select @maLopCu = MaLop from deleted
+		select @maLopMoi = MaLop from inserted
+		select @siSoLopCu = COUNT(MaSV) from SINHVIEN where MaLop = @maLopCu
+		select @siSoLopMoi = COUNT(MaSV) from SINHVIEN where MaLop = @maLopMoi
+		update LOP set SiSo = @siSoLopCu where MaLop = @maLopCu
+		update LOP set SiSo = @siSoLopMoi where MaLop = @maLopMoi
+	end
+
+select * from Lop
+select * from SinhVien
+update SinhVien set MaLop = 'L01' where MaSV = 'SV02'
+select * from Lop
+select * from SinhVien
